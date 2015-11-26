05a8a3090451932f899f1605b57c762b7ae0fa44
whereiscoire <whereiscoire@gmail.com>
2015-11-26 01:53:19 -0500

    added normalize, scrolling and logo fixes on mobile.

--- js/script.js
+++ js/script.js
@@ -12,4 +12,18 @@ $(document).ready(function(){
   $("#skills").click(function(){
     $("#myskills").slideToggle("slow");
   });
+
+  $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
+  
 });
